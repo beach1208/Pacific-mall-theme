fdcef1e0a2ac913dfef1218dca49234cf3d00c04
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-02-20 15:10:54 -0800

    header,footer,functions.php added and custom header added

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+//custome header
+add_theme_support(
+    'custom-header',
+    array(
+        'width' => 950,
+        'height' => 295,
+        'header-text' => false,
+      
+    )
+);
