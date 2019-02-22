730845ef8d0cf2203105ab8424d3fdc27c3b88a9
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-02-21 16:11:37 -0800

    column and articles added on front page

--- functions.php
+++ functions.php
@@ -18,3 +18,21 @@ register_nav_menus(
         'place_utility' => 'Utility'
     )
 );
+
+//add eye catch images
+add_theme_support('post-thumbnails');
+
+//set size of thumbnails
+set_post_thumbnail_size(90,90,true);
+
+//side bar image size
+add_image_size('small_thumbnail',61,61,true);
+
+//archive thumbnail size
+add_image_size('large_thumbnail',120,120,true);
+
+//sub header image size
+add_image_size('category_image',658,113,true);
+
+//mall image size
+add_image_size('pickup_thumbnail',302,123,true);
